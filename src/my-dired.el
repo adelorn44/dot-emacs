e0e439adda059205b6f21b223d55686850a58289
Jules Sagot <jules.sagot@gmail.com>
2021-08-09 14:59:43 +0200

    Dired: fonction pour garder uniquement les fichiers d'une extension
    Si le point est sur test.py, on gardera tous les fichiers finissant
    par .py et on masquera les autres. Les dossiers sont gardés par
    défaut.

--- src/my-dired.el
+++ src/my-dired.el
@@ -3,7 +3,8 @@
 (use-package dired
   :bind
   (:map dired-mode-map
-   ("w" . my-dired-copy-filename-as-kill)))
+	("w" . my-dired-copy-filename-as-kill)
+	("C-c C-c" . my-dired-keep-extensions-at-point)))
 
 (defun my-dired-copy-filename-as-kill (&optional arg)
   "By default, return the absolute path."
@@ -12,5 +13,22 @@
       (dired-copy-filename-as-kill arg)
     ;; 0 means kill the absolute path
     (dired-copy-filename-as-kill 0)))
+
+(defun my-dired-keep-extensions-at-point (&optional arg)
+  "Keep only the files with the same extension as at point. With any args, do not keep directories."
+  (interactive "P")
+  (if (not (string-equal major-mode "dired-mode")) (error "Not in Dired"))
+  (let* ((filename (dired-copy-filename-as-kill)) (extension-regexp "\\(\\.[a-zA-Z]+\\)+") extension)
+    (if (not filename) (error "No file at point"))
+    (if (string-match-p extension-regexp filename)
+	(progn
+	  (setq extension (substring filename (string-match extension-regexp filename) (match-end 0)))
+	  (revert-buffer)
+	  (dired-unmark-all-marks)
+	  (dired-mark-files-regexp (concat (regexp-quote extension) "$"))
+	  (dired-toggle-marks)
+	  (if (not arg) (dired-mark-directories t))
+	  (dired-do-kill-lines))
+      (message "This file has not any extension or is a directory"))))
 
 (provide 'my-dired)
